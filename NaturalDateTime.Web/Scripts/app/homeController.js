6a805efc3eef6ef37b75ca7a1a4ff0710946628a
Darko Sancanin <darko.sancanin@gmail.com>
2015-06-26 22:37:01 +1000

    Changed example to one that has DST.

--- NaturalDateTime.Web/Scripts/app/homeController.js
+++ NaturalDateTime.Web/Scripts/app/homeController.js
@@ -17,7 +17,7 @@ app.controller('naturalDateTime').controller('homeController', ["$scope", "$http
           });
     };
 
-    var randomQuestions = ["e.g. Whats the time in Sydney when it's 10:30PM in New York?", "e.g. When does daylight saving time start in Japan?", "e.g. If its 8pm in Cleveland, whats the time in California?", "e.g. When does daylight saving time start in 2016 in Chicago?"];
+    var randomQuestions = ["e.g. Whats the time in Sydney when it's 10:30PM in New York?", "e.g. When does daylight saving time start in Nottingham?", "e.g. If its 8pm in Cleveland, whats the time in California?", "e.g. When does daylight saving time start in 2016 in Chicago?"];
     var randomIndex = Math.floor(Math.random() * (randomQuestions.length));
     $scope.placeholder = randomQuestions[randomIndex];
 }]);
