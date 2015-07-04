ae938a7d15e07b68fd616a9ad10f685be85e14ce
Darko Sancanin <darko.sancanin@gmail.com>
2015-07-04 22:31:15 +1000

    Added option to pass client and client_version to home controller.

--- NaturalDateTime.Web/Controllers/HomeController.cs
+++ NaturalDateTime.Web/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace NaturalDateTime.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string q, string debug)
+        public ActionResult Index(string q, string client, string client_version, string debug)
         {
             ViewBag.Title = "Natural Date and Time";
             if (!string.IsNullOrEmpty(q))
@@ -20,7 +20,9 @@ namespace NaturalDateTime.Web.Controllers
                 if (Request.Headers["User-Agent"] != null)
                     userAgent = Request.Headers["User-Agent"].ToString();
                 var questionService = new QuestionService();
-                var answer = questionService.GetAnswer(q, userAgent, ApplicationSettings.WebClientName, ApplicationSettings.WebApplicationVersion);
+                if (string.IsNullOrEmpty(client)) client = ApplicationSettings.WebClientName;
+                if (string.IsNullOrEmpty(client_version)) client_version = ApplicationSettings.WebApplicationVersion;
+                var answer = questionService.GetAnswer(q, userAgent, client, client_version);
                 return View("Index", new HomeViewModel(q, answer.AnswerText, answer.Note));
             }
 
